0b91a391a216abf494c20c68213e11ebd90063f2
DanielWojda <contact@danielwojda.org>
2015-06-27 09:50:30 +0200

    Add Mustache.

--- src/main/java/pl/com/bms/UberController.java
+++ src/main/java/pl/com/bms/UberController.java
@@ -3,12 +3,15 @@ package pl.com.bms;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 
 @Controller
 class UberController {
 
    @RequestMapping("/")
-   public String index() {
+   public String index(Map<String, String> model) {
+      model.put("message", "hej BMS!!!");
       return "index";
    }
 }
